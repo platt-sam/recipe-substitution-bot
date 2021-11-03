4305a081c2af2a329ace16a0e92b8a9e8c2c23ec
Sam Platt <samplatt@Sams-Macbook.local>
2021-11-02 20:43:47 -0700

    implemented create_list_of_foods function

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import argparse, os, random, requests
-from os import exists
-import bs4 as BeautifulSoup
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
+from bs4 import BeautifulSoup
 
 def create_list_of_foods() -> list:
     '''
@@ -10,9 +7,24 @@ def create_list_of_foods() -> list:
 
     note to future Sam: this is the tricky part
     '''
+    food_files = os.listdir("wiktionary") # list of filenames
+    if (food_files[0] == '.DS_Store'): # remove .DS_Store file from the list if it exists
+        food_files.pop(0)
 
+    list_of_foods = list() # list of food words
 
-    return list()
+    for food_file in food_files:
+        fn = 'wiktionary/' + food_file
+        
+        with open(fn) as fp:
+            content = fp.read()
+            fp.close()
+            soup = BeautifulSoup(content, 'html.parser')
+
+            for li in soup.find(id="mw-pages").find_all('li'):
+                list_of_foods.append(li.find('a')['title'])
+        
+    return(list_of_foods)
 
 def find_ingredients_in_recipe(filename: str) -> list:
     '''
@@ -29,14 +41,14 @@ def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-fn', '--filename', type=str, help='the filename of the txt file containing the recipe')
     args = parser.parse_args()
-    src_filename = args.filename
+    src_filename = str(args.filename)
 
-    if (exists(src_filename) == False):
+    if (os.path.exists(src_filename) == False):
         raise Exception('File {fn} not found'.format(fn=src_filename))
 
     foods = create_list_of_foods()
 
-    recipe_ingredients = find_ingredients_in_recipe(src_filename)
+    # recipe_ingredients = find_ingredients_in_recipe(src_filename)
 
 
 
