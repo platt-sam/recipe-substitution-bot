d16b7b68ddc8795fb72f8ec00b4e9561cdcd287e
Sam Platt <samplatt@sams-macbook.home>
2021-11-02 23:40:11 -0700

    initial working version

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def create_list_of_foods() -> list:
     if food_files.count('.DS_Store') >= 1:
         food_files.remove('.DS_Store') 
 
-    list_of_foods = list() # list of food words
+    list_of_foods = list()
 
     for food_file in food_files:
         with open('wiktionary/' + food_file) as fp:
@@ -21,11 +21,9 @@ def create_list_of_foods() -> list:
                 food = li.find('a')['title']
                 if list_of_foods.count(food) <= 0: # make sure there are no duplicates
                     list_of_foods.append(food)
-        
-    return(list_of_foods)
+    return list_of_foods
 
-# TODO
-def find_ingredients_in_recipe(filename: str, list_of_foods: list) -> list:
+def find_ingredients_in_recipe(list_of_foods: list, filename: str) -> list:
     '''
     Takes the name of the file containing the recipe as an argument, converts the contents of the file to string format, 
     and returns the contents of the file as a string, returns a list of all ingredients found in the recipe
@@ -33,23 +31,23 @@ def find_ingredients_in_recipe(filename: str, list_of_foods: list) -> list:
     with open(filename) as fp:
         content = fp.read()
         fp.close()
-    
+
     list_of_ingredients = list()
 
     for food in list_of_foods:
         if content.find(food) != -1:
             list_of_ingredients.append(food)
-
     return list_of_ingredients
 
-def pick_ingredient_to_be_replaced(ingredients: list) -> str:
-    return random.choice(ingredients)
+def pick_random_ingredient(l: list) -> str:
+    return random.choice(l)
 
 def generate_comment(original_ingredient: str, new_ingredient: str) -> str:
-    
-    return str()
+    comment = "This turns out so much better if you swap out the {oi} for {ni}. Sounds really weird but trust me".format(oi=original_ingredient, ni=new_ingredient)
+    return comment
 
 def main():
+
     # parsing the arguments
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-fn', '--filename', type=str, help='the filename of the txt file containing the recipe')
@@ -61,13 +59,16 @@ def main():
         raise Exception('File {fn} not found'.format(fn=src_filename))
 
     list_of_foods = create_list_of_foods()
-    print(len(list_of_foods))
+    # print(len(list_of_foods)) # the total number of foods in list_of_foods
 
-    list_of_ingredients = find_ingredients_in_recipe(src_filename, list_of_foods)
+    list_of_ingredients = find_ingredients_in_recipe(list_of_foods, src_filename)
 
-    # print(list_of_ingredients)
-
-
+    if len(list_of_ingredients) < 3:
+        for ingredient in list_of_ingredients:
+            print(generate_comment(ingredient, pick_random_ingredient(list_of_foods)))
+    else:
+        for x in range(3):
+            print(generate_comment(pick_random_ingredient(list_of_ingredients), pick_random_ingredient(list_of_foods)))
 
 if __name__ == "__main__":
     main()
