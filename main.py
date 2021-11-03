8e80efa0c72f227235424e109f1fb3cb1b6318ee
Sam Platt <samplatt@sams-macbook.home>
2021-11-02 23:17:13 -0700

    getting ingredients from recipe, other related functions

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 
 def create_list_of_foods() -> list:
     '''
-    Creates a list of food words from wiktionary.org category en:Foods using "beautiful soup"
+    Creates a list of food words from wiktionary.org category pages using "beautiful soup"
     '''
     food_files = os.listdir("wiktionary") # list of filenames
-    if (food_files[0] == '.DS_Store'): # remove .DS_Store file from the list if it exists
-        food_files.pop(0)
+    if food_files.count('.DS_Store') >= 1:
+        food_files.remove('.DS_Store') 
 
     list_of_foods = list() # list of food words
 
@@ -24,32 +24,48 @@ def create_list_of_foods() -> list:
         
     return(list_of_foods)
 
-def find_ingredients_in_recipe(filename: str) -> list:
+# TODO
+def find_ingredients_in_recipe(filename: str, list_of_foods: list) -> list:
     '''
     Takes the name of the file containing the recipe as an argument, converts the contents of the file to string format, 
     and returns the contents of the file as a string, returns a list of all ingredients found in the recipe
     '''
-    return list()
+    with open(filename) as fp:
+        content = fp.read()
+        fp.close()
+    
+    list_of_ingredients = list()
+
+    for food in list_of_foods:
+        if content.find(food) != -1:
+            list_of_ingredients.append(food)
+
+    return list_of_ingredients
 
 def pick_ingredient_to_be_replaced(ingredients: list) -> str:
     return random.choice(ingredients)
 
+def generate_comment(original_ingredient: str, new_ingredient: str) -> str:
+    
+    return str()
 
 def main():
+    # parsing the arguments
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-fn', '--filename', type=str, help='the filename of the txt file containing the recipe')
     args = parser.parse_args()
     src_filename = str(args.filename)
 
+    # check if input file exists
     if (os.path.exists(src_filename) == False):
         raise Exception('File {fn} not found'.format(fn=src_filename))
 
-    foods = create_list_of_foods()
-    print(len(foods))
+    list_of_foods = create_list_of_foods()
+    print(len(list_of_foods))
 
-    # recipe_ingredients = find_ingredients_in_recipe(src_filename)
+    list_of_ingredients = find_ingredients_in_recipe(src_filename, list_of_foods)
 
-
+    # print(list_of_ingredients)
 
 
 
