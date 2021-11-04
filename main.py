84e068c044a2bdc21f5a8b2c06c5129229b2e50d
Sam Platt <samplatt@Sams-Macbook.local>
2021-11-04 11:14:28 -0700

    now using live requests and filtering

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-import argparse, os, random
+import argparse, os, random, requests, wordfilter
 from bs4 import BeautifulSoup
+from wordfilter import Wordfilter
 
 def create_list_of_foods() -> list:
     '''
     Creates a list of food words from wiktionary.org category pages using "beautiful soup"
     '''
-    food_files = os.listdir("wiktionary") # list of filenames
-    if food_files.count('.DS_Store') >= 1:
-        food_files.remove('.DS_Store') 
 
     list_of_foods = list()
 
-    for food_file in food_files:
-        with open('wiktionary/' + food_file) as fp:
-            content = fp.read()
-            fp.close()
-            soup = BeautifulSoup(content, 'html.parser')
+    with open('wiktionary_categories.txt') as fp:
+        categories = fp.read()
+        fp.close()
+        
+    for category in categories.splitlines():
+        r = requests.get(category, timeout=5)
+        r.encoding = 'utf-8'
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-            for li in soup.find(id="mw-pages").find_all('li'):
-                food = li.find('a')['title']
-                if list_of_foods.count(food) <= 0: # make sure there are no duplicates
-                    list_of_foods.append(food)
+        for li in soup.find(id="mw-pages").find_all('li'):
+            food = li.find('a')['title']
+            if list_of_foods.count(food) <= 0: # make sure there are no duplicates
+                list_of_foods.append(food)
+
+    return filter_list_of_foods(list_of_foods)
+
+def filter_list_of_foods(list_of_foods: list) -> list:
+    wordfilter = Wordfilter()
+    wordfilter.addWords(['arse', 'baby beef', "breast milk", 'cow', 'cup', 'cuppa', 'fuck', 'good egg', 'heal-all', 'human burger', 'lacta', 'latte art', "mother's milk", 'sarcophagic', 'sessionable', 'shit', 'take the gilt off the gingerbread', 'tea cosy', 'tea house', 'tea room', 'turd']) # add additional words to filter
+    
+    for food in list_of_foods:
+        if (wordfilter.blacklisted(food) == True): # if the food contains a blacklisted word
+            list_of_foods.remove(food) # remove that food from the list of foods
     return list_of_foods
 
 def find_ingredients_in_recipe(list_of_foods: list, filename: str) -> list:
@@ -56,7 +67,13 @@ def main():
         raise Exception('File {fn} not found'.format(fn=src_filename))
 
     list_of_foods = create_list_of_foods()
-    # print(len(list_of_foods)) # the total number of foods in list_of_foods
+    
+    # for filtering
+    print(str(len(list_of_foods)) + " food words found") # the total number of foods in list_of_foods
+    for x in range(10):
+        print(random.choice(list_of_foods))
+    exit()
+    
 
     list_of_ingredients = find_ingredients_in_recipe(list_of_foods, src_filename)
 
